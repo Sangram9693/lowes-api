12842ac6c302f43846e750a7b77012baaf5c4bbf
SangramBadi <sangram.badi@comviva.com>
2021-07-08 16:32:10 +0530

    Initial commit

---
+++ src/main/java/com/urlshort/lowes/LowesApplication.java
@@ -0,0 +1,13 @@
+package com.urlshort.lowes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowesApplication.class, args);
+	}
+
+}
